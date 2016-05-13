542cff02842ea40d4dc8e6894add1855894a4957
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-05-13 18:19:44 +0900

    Add react-bootstrap

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
-export default class App extends React.Component {
+class App extends React.Component {
 
   render() {
     return (
       <div>
-        Hello, React
+        <ButtonToolbar>
+          <Button>Defaulr</Button>
+          <Button bsStyle="primary">Primary</Button>
+          <Button bsStyle="success">Success</Button>
+          <Button bsStyle="info">Info</Button>
+          <Button bsStyle="warning">Warning</Button>
+          <Button bsStyle="danger">Danger</Button>
+        </ButtonToolbar>
       </div>
     );
   }
   
 }
+
+export default App;
--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+require('../node_modules/bootstrap/dist/css/bootstrap.css')
 
 import App from './components/app'
 
